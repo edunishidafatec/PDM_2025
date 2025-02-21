364ef49c86a0672121b0c886295df5ddfd42f44e
Eduardo Gomes <eduardogmsnishida1@gmail.com>
2025-02-21 17:37:32 -0300

    Encerra a apostila 03 21/02

--- primeiro-app/src/App.jsx
+++ primeiro-app/src/App.jsx
@@ -1,20 +1,27 @@
 // Parece html, porem ele está dentro do JS
+import './styles.css'
 function App (){
+  const estiloBotao = {marginTop: 12, paddingBottom: 8, backgroundColor: "blueviolet", color:"white", border: "none", width:"100%", borderRadius: 8  }
+  const textoDoRotulo = 'Nome:'
+  const obterTextoDoBotao = () => 'Enviar'
+  const aoClicarNoBotao = () => alert('Clicou')
   return (
   <div
   style={{margin: 'auto', width: 768, backgroundColor: '#EEE', padding: 12, borderRadius: 8 }}>
       <label 
       htmlFor="nome"
+      className='rotulo'
       style={{display: 'block', marginBottom: 4}}>
-        Nome:
+        {textoDoRotulo}
       </label>
       <input
       type="text"
       id="none"
       style={{paddingTop: 8, paddingBottom: 8, borderStyele: 'hidden', width: '100%', borderRadius: 8, outline:'none'}}/>
   <button
-  style={{marginTop: 12, paddingBottom: 8, backgroundColor: "blueviolet", color:"white", border: "none", width:"100%", borderRadius: 8  }}>
-  Enviar
+  onClick={aoClicarNoBotao} // Poderia fazer direto aqui, sem uso de const
+  style={estiloBotao}>
+  {obterTextoDoBotao()}
   </button>
       </div>
   )
