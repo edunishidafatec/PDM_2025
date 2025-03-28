cc3b63a2dc49a073e3a29225007f559c78a1b67c
Eduardo Gomes <eduardogmsnishida1@gmail.com>
2025-03-28 15:25:05 -0300

    Learn(ap07-react): encerra a pagina 6

---
+++ exibe-fotos/src/components/Busca.jsx
@@ -0,0 +1,26 @@
+// rce
+import React, { Component } from 'react'
+import { IconField } from 'primereact/iconfield'
+import { InputIcon } from 'primereact/inputicon'
+import { InputText } from 'primereact/inputtext'
+export class Busca extends Component {
+    state = {
+        termoDeBusca: ''
+    }
+  render() {
+    return (
+      <div
+            className='flex flex-column'>
+                <IconField iconPosition='left'>
+                    <InputIcon className='pi pi-search'></InputIcon>
+                    <InputText placeholder="Busca algo..." />
+                </IconField>
+            
+            
+
+      </div>
+    )
+  }
+}
+
+export default Busca
